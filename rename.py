2e3b68300ad5c82b837da777ebd100be761d26da
greg <greg3@tutamail.com>
2023-12-21 09:47:10 -0300

    chore: Improved logic of the script

--- rename.py
+++ rename.py
@@ -8,9 +8,11 @@ def main():
     files = glob("path/to/dir/**/*")
 
     for file in files:
-        if "," in file:
-            print(f" => {file!r};")
-            Path(file).rename(file.replace(",", "-"))
+        # if basename(__file__) in file:
+        if not isfile(file) or "," not in file:
+            continue
+        print(f" => {file!r};")
+        Path(file).rename(file.replace(",", "-"))
 
 
 if __name__ == "__main__":
